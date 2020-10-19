96afb983b7b7e5e0b91da341b5621261e0d2945d
royliao <royliao88@gmail.com>
2020-10-19 19:24:01 +0800

    Navigator 2.0

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import MainNavigator from './src/navigation/MainNavigator'
+export default function App() {
+  return <MainNavigator />
+}
+/*
 function HomeScreen({ navigation }) {
 
   return (
@@ -12,12 +17,34 @@ function HomeScreen({ navigation }) {
       <Text>Home Screen</Text>
 
       <Button
-
         title="Go to Details"
-
         onPress={() => navigation.navigate('Details')}
-
       />
+      <Button
+        title="Go to test"
+        onPress={() => navigation.navigate('Test')}
+      />
+
+    </View>
+
+  );
+
+}
+function TestScreen({ navigation }) {
+
+  return (
+
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+      <Text>Test Screen</Text>
+
+      <Button
+
+      title="Go to Home"
+
+      onPress={() => navigation.navigate('Home')}
+
+    />
 
     </View>
 
@@ -53,13 +80,13 @@ export default function App() {
 
       <Stack.Navigator>
 
-        <Stack.Screen name="Home" component={HomeScreen} />
-
         <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Test" component={TestScreen} />
 
       </Stack.Navigator>
 
     </NavigationContainer>
 
   );
-}
+}*/
---
+++ src/screens/Index.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import {Alert, Button, View, Text} from 'react-native';
+
+function Index(){
+    return(
+        <View>
+            <Text>good</Text>
+        </View>
+    )
+}
+export default Index
