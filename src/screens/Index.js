741b6b4ffabb455a96f435531a1cf0ec9c1596d4
royliao <royliao88@gmail.com>
2020-10-31 23:14:24 +0800

    導入flexbox

--- src/screens/Index.js
+++ src/screens/Index.js
@@ -15,21 +15,42 @@ import {
 } from '@expo/vector-icons';
 function Index() {
     return (
-        <View style={{ backgroundColor: '#46A3FF',height: '100%'  }}>
-            <View style={{ flexDirection: 'row', backgroundColor: 'white', height: '7%' }}>
-                <Text style={{ fontWeight: 'bold', color: '#0072E3', fontSize: 16}}>編輯個人檔案</Text>
-                <Entypo name="pencil" style={styles.topIcon}/>
-                <Entypo name="log-out" style={styles.topIcon}/>
-                <AntDesign name="notification" style={styles.topIcon}/>
+
+        <View style={styles.container}>
+            <View style={styles.indexTop}>
+                <View style={{flex:1,flexDirection: 'row',justifyContent: 'center',alignItems:'center',}}>
+                    <Text style={{ fontWeight: 'bold', color: '#0072E3',fontSize:14}}>編輯個人檔案</Text>
+                    <Entypo name="pencil" style={styles.topIcon} />
+                </View>
+                <View style={{flex:1,flexDirection: 'row',justifyContent: 'flex-end',alignItems:'center'}}>
+                    <Entypo name="log-out" style={styles.topIcon} />
+                    <AntDesign name="notification" style={styles.topIcon} />
+                </View>
+            </View>
+            <View style={styles.row}>
+                <Text>Row-2</Text>
             </View>
         </View>
     )
 }
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ccc',
+    },
+    indexTop:{
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor:'white'
+    },
+    row: {
+        flex: 17,
+        flexDirection: 'row'
+    },
     topIcon:{
-        color:'gray',
-        fontSize:24,
-    }
-  });
-  
+        fontSize:16,
+        color:'gray'
+
+    },
+})
 export default Index
