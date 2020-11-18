4fe363597045297f8236041ff7cce011a31baf76
royliao <royliao88@gmail.com>
2020-11-18 11:03:05 +0800

    添加卡片(有重疊問題)

--- src/screens/Index.js
+++ src/screens/Index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Alert, Button, View, Text, StyleSheet,Image } from 'react-native';
+import { Alert, Button, View, Text, StyleSheet, Image, Card } from 'react-native';
 import {
     AntDesign,
     Ionicons,
@@ -18,34 +18,36 @@ function Index() {
 
         <View style={styles.container}>
             <View style={styles.indexTop}>
-                <View style={{flex:1}}>
+                <View style={{ flex: 1 }}>
                 </View>
-                <View style={{flex:14,flexDirection: 'row',justifyContent: 'flex-start',alignItems:'center',}}>
-                    <Text style={{marginRight:4,fontWeight: 'bold', color: '#0072E3',fontSize:14}}>編輯個人檔案</Text>
+                <View style={{ flex: 14, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', }}>
+                    <Text style={{ marginRight: 4, fontWeight: 'bold', color: '#0072E3', fontSize: 14 }}>編輯個人檔案</Text>
                     <Foundation name="clipboard-pencil" style={styles.topIcon} />
                 </View>
-                <View style={{flex:1,flexDirection: 'row',justifyContent: 'center',alignItems:'center'}}>
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
                     <Entypo name="log-out" style={styles.topIcon} />
                 </View>
-                <View style={{flex:1,flexDirection: 'row',justifyContent: 'center',alignItems:'center'}}>
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
                     <AntDesign name="notification" style={styles.topIcon} />
                 </View>
-                <View style={{flex:1}}>
+                <View style={{ flex: 1 }}>
                 </View>
             </View>
             <View style={styles.index}>
                 <View style={styles.indexLogo}>
                     <Image
-                    height='80%'
-                    width='80%' 
-                    source={require('../../Image/logo.png')} 
+                        height='100%'
+                        width='100%'
+                        source={require('../../Image/logo.png')}
                     />
                 </View>
-                <View style={styles.indexLogo}>
-                    
+                <View style={styles.indexCard}>
+                    <View style={{flex:1}}></View>
+                    <View style={{flex:15,backgroundColor:'white',borderRadius: 10,}}></View>
+                    <View style={{flex:1}}></View>
                 </View>
-                <View style={styles.indexLogo}>
-                    
+                <View style={styles.indexButton}>
+
                 </View>
             </View>
         </View>
@@ -56,23 +58,32 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#ccc',
     },
-    indexTop:{
+    indexTop: {
         flex: 1,
         flexDirection: 'row',
-        backgroundColor:'white'
+        backgroundColor: 'white'
     },
     index: {
         flex: 17,
         flexDirection: 'column',
-        backgroundColor:'#66B3FF'
+        backgroundColor: '#66B3FF'
     },
-    topIcon:{
-        fontSize:20,
-        color:'gray'
+    topIcon: {
+        fontSize: 20,
+        color: 'gray'
     },
-    indexLogo:{
-        flex:1,
+    indexLogo: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    indexCard: {
+        flex: 1.5,
+        flexDirection: 'row',
+    },
+    indexButton:{
+        flex:3,
         flexDirection:'row'
     }
+
 })
 export default Index
